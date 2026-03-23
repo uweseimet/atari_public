f9f6696b6996e6ab22dd24d224572d74beff243f
Uwe Seimet <Uwe.Seimet@seimet.de>
2026-03-23 08:57:19 +0100

    Update test suite version

--- SDRVTEST/SDRVMAIN.C
+++ SDRVTEST/SDRVMAIN.C
@@ -1,8 +1,8 @@
-/***********************************/
-/* SCSI Driver/Firmware Test 3.00ž */
-/*                                 */
-/* (C) 2014-2026 Uwe Seimet        */
-/***********************************/
+/**********************************/
+/* SCSI Driver/Firmware Test 3.00 */
+/*                                */
+/* (C) 2014-2026 Uwe Seimet       */
+/**********************************/
 
 
 #include <string.h>
@@ -84,7 +84,7 @@ main()
 		return -1;
 	}
 
-	print("SCSI Driver and firmware test V3.00ž\n");
+	print("SCSI Driver and firmware test V3.00\n");
 	print("½ 2014-2026 Uwe Seimet\n\n");
 
 	if(getNvm(&nvm)) {
